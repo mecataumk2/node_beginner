0d845b757ff7a56e19812063fc11ea97f68c7fc9
SeungDae Lee <mecataumk2@gmail.com>
2017-06-09 18:24:38 +0900

    File upload 구현 중.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,61 @@
 const exec = require("child_process").exec;
+const querystring = require("querystring");
+const fs = require("fs");
 
 function start(response){
     console.log("Request handler 'start' was called.");
-    exec("ls -al", (error, stdout, stderr) => {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
-        response.end();
-    });
+    // exec("find /",
+    // { timeout: 10000, maxBuffer: 20000*1024 },
+    // function (error, stdout, stderr) {
+    //   response.writeHead(200, {"Content-Type": "text/plain"});
+    //   response.write(stdout);
+    //   response.end();
+    // });
+
+    // exec("ls -al", (error, stdout, stderr) => {
+    //     response.writeHead(200, {"Content-Type": "text/plain"});
+    //     response.write(stdout);
+    //     response.end();
+    // });
+
+    const body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload File" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload(response){
+function upload(response, postData){
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write(`You've send the text : ${querystring.parse(postData).text}`);
     response.end();
+}
+
+function show(response){
+    console.log("Request handler 'show' was called.");
+    fs.readFile("/tmp/test.png", "binary", (error, file) => {
+        if (error){
+            response.writeHead("500", {"Content-Type": "text/plain"});
+            response.write(`${error} \n`);
+            response.end();            
+        } else { 
+            response.writeHead("200", {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const http = require("http");
 const url = require("url");
 
+const hostname = "192.168.0.190";
 const port = 8412;
 
 function start(route, handle){    
-    function onRequest(request, response){
-        const pathName = url.parse(request.url).pathname;
+    function onRequest(request, response){        
         const query = url.parse(request.url).query;
+        let postData = "";
+        const pathname = url.parse(request.url).pathname;
         console.log(`Request received : ${request.url}`);
-        console.log(`Path name : ${pathName}`);
+        console.log(`Path name : ${pathname}`);
         console.log(`Query : ${query}`);
 
-        route(handle, pathName, response);
+        request.setEncoding("utf8");
+
+        request.addListener("data", (postDataChunk) => {
+            postData += postDataChunk;
+            console.log(`Recieved POST data chunk '${postDataChunk}'.`);
+        });
+
+        request.addListener("end", () => {
+            route(handle, pathname, response, postData);    
+        });
 
         // response.writeHead(200, {"Content-Type": "text/plain"});
         // response.write("Hello World");
         // response.end();
     }
-    http.createServer(onRequest).listen(port);    
+    http.createServer(onRequest).listen(port, hostname);    
     console.log("Server hats started.");
 }
 
